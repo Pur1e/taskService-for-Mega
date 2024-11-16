4269fd4df5716a2d5749b67d2f4cf8193f7a5ee2
Dosya <abdk4lyk0v@gmail.com>
2024-11-16 17:09:11 +0600

    - init project - server configuration provided - added table "tasks" to H2 with Liquibase migration

---
+++ src/main/java/kg/com/restapifortaskmanagement/RestApiForTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package kg.com.restapifortaskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForTaskManagementApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiForTaskManagementApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/kg/com/restapifortaskmanagement/RestApiForTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.com.restapifortaskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForTaskManagementApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
